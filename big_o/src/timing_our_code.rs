6b5dfbac00d96988b4d9c4f9fc818e1d7bff1fd8
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-05-19 03:47:32 +0100

    Orgnanized tests into modules

--- big_o/src/timing_our_code.rs
+++ big_o/src/timing_our_code.rs
@@ -1,10 +1,10 @@
 fn _add_up_to(n: u32) -> u32 {
     let mut total = 0;
-    
+
     for i in 0..=n {
         total += i;
     }
-    
+
     total
 }
 
@@ -12,11 +12,21 @@ fn _add_up_too(n: u32) -> u32 {
     n * (n + 1) / 2
 }
 
-#[test]
-fn test_sample() {
-    let result = _add_up_to(6);
-    let res = _add_up_too(6);
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sample() {
+        let result = _add_up_to(6);
         
-    assert_eq!(res, 21);
-    assert_eq!(result, 21);
+        assert_eq!(result, 21);
+    }
+
+    #[test] 
+    fn should_return_21() {
+        let result = _add_up_too(6);
+
+        assert_eq!(result, 21);
+    }
 }
