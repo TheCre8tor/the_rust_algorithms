e514f4e085b385bf7164f10c692910b708515b76
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-05-24 05:20:32 +0100

    learnt the basics of logarithms

--- big_o/src/logarithms.rs
+++ big_o/src/logarithms.rs
@@ -1,2 +1,7 @@
 // log₂(8) = 3 --> 2³ = 8
 // log₂(value) = exponent --> 2ᵉˣᵖᴼⁿᵉⁿᵗ = value
+
+/* The logarithm of a number roughtly measures the
+   number of times you can divide that number by 2
+   before you get a value that's less than or equal
+   to one. */
