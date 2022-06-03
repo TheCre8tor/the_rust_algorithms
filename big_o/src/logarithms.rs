7e4f89364c0ed02a6bf387188b87fe0f5942b5ff
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-06-03 21:33:21 +0100

    Commited all codes

--- big_o/src/logarithms.rs
+++ big_o/src/logarithms.rs
@@ -1,10 +1,8 @@
 // log₂(8) = 3 --> 2³ = 8
 // log₂(value) = exponent --> 2ᵉˣᵖᴼⁿᵉⁿᵗ = value
 
-// ? I will come back to this later -->
+//? I will come back to this later -->
 /* The logarithm of a number roughtly measures the
    number of times you can divide that number by 2
    before you get a value that's less than or equal
    to one. */
-
-   // cargo new --bin objects_and_arrays
