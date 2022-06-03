654a9780c7b4f9c7b71b3478f53fe9a3c4e8be4d
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-06-03 18:52:27 +0100

    Lecture on object

---
+++ objects_and_arrays/src/main.rs
@@ -0,0 +1,31 @@
+fn main() {
+    /* SECTION  Big O of Objects
+    1. Insertion -> O(1)
+    2. Removal -> O(1)
+    3. Searching -> O(N)
+    4. Access -> O(1)
+
+    When you don't need any ordering,
+    objects are an excellent choice!
+    */
+
+    #[derive(Debug)] 
+    struct Instructor {
+        first_name: String,
+        is_instructor: bool,
+        favourite_number: Vec<u8>,
+    }
+
+    let mut instructor = Instructor {
+        first_name: String::from("Kelly"),
+        is_instructor: true,
+        favourite_number: vec![1, 2, 3, 4],
+    };  
+
+    // Insertion -->
+    instructor.favourite_number.push(5);
+    instructor.is_instructor = false;
+    instructor.first_name = "Alexander".to_owned();
+    
+    println!("{:?}", instructor);
+}
