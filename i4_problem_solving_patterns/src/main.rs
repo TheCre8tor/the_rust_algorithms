d3c7448c216ad400ab0e876adf7a4cc99e3388d8
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-06-12 16:10:54 +0100

    Listed Problem solving patterns

--- i4_problem_solving_patterns/src/main.rs
+++ i4_problem_solving_patterns/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
     println!("Hello, world!");
+
+    /* HOW DO YOU IMPROVE IN PROBLEM SOLVING?
+       1. Devise a plan for solving problems
+       * 2. Master common problem solving patterns
+
+       ? SOME PATTERNS...
+         - Frequency Counter
+         - Multiple Pointers
+         - Sliding Window
+         - Divide and Conquer
+         - Dynamic Programming
+         - Greedy Algorithms
+         - Backtracking
+         - Many more!...
+    */
 }
