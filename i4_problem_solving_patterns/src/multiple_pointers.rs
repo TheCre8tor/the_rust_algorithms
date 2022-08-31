f8a69d753f52d4c0ad77bdae32f359cfd43c376f
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-08-31 10:19:25 +0100

    Learning Multiple Pointers pattern

--- i4_problem_solving_patterns/src/multiple_pointers.rs
+++ i4_problem_solving_patterns/src/multiple_pointers.rs
@@ -1,1 +1,12 @@
-pub fn run() {}
+pub fn run() {
+    /* ***********************************
+      * Multiple Pointers ->
+      Creating pointers or values that correspond to an index
+      or position and move towards the beginning, end or middle
+      based on a certain condition.
+
+      Very efficient for solving problems with minimal space
+      complexity.
+
+    */
+}
