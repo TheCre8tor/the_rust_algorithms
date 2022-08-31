02cce17db2cdacd92c3a11ea389df4212d4b9ee8
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-08-31 10:56:26 +0100

    Working on solving sumZero problem

--- i4_problem_solving_patterns/src/multiple_pointers.rs
+++ i4_problem_solving_patterns/src/multiple_pointers.rs
@@ -7,6 +7,24 @@ pub fn run() {
 
       Very efficient for solving problems with minimal space
       complexity.
-
     */
+
+    /* ***********************************
+      * Example No. 1:
+      Write a function called sumZero which accepts a sorted
+      array or integers. The function should find the first pair
+      where the sum is 0. Return an array that includes both
+      values that sum to zero or undefined if a pair does name_two
+      exist.
+
+      sumZero([-3, -2, -1, 0, 1, 2, 3]) -> [-3, 3]
+      sumZero([-2, 0, 1, 3]) -> undefined
+      sumZero([1, 2, 3]) -> undefined
+    */
+}
+
+/// This function result to `O(N)` Time Complexity, and `0(1)` Space Complexity
+fn sumZero(arr: Vec<u8>) -> Vec<u8> {
+    let left: usize = 0;
+    let right: usize = arr.len() - 1_usize;
 }
