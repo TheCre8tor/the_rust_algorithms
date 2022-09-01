da12068dcc7df47e18daa8b1ea7a2ad652efd3c2
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-09-01 04:33:57 +0100

    Glanced through multiple pointers

--- i4_problem_solving_patterns/src/multiple_pointers.rs
+++ i4_problem_solving_patterns/src/multiple_pointers.rs
@@ -14,17 +14,38 @@ pub fn run() {
       Write a function called sumZero which accepts a sorted
       array or integers. The function should find the first pair
       where the sum is 0. Return an array that includes both
-      values that sum to zero or undefined if a pair does name_two
+      values that sum to zero or [] if a pair does name_two
       exist.
 
       sumZero([-3, -2, -1, 0, 1, 2, 3]) -> [-3, 3]
-      sumZero([-2, 0, 1, 3]) -> undefined
-      sumZero([1, 2, 3]) -> undefined
+      sumZero([-2, 0, 1, 3]) -> []
+      sumZero([1, 2, 3]) -> []
     */
+
+    let result: Vec<i8> = sum_zero(vec![-4, -6, -1, 0, 1, 2, 3]);
+
+    println!("Sum Zero: {:?}", result);
 }
 
 /// This function result to `O(N)` Time Complexity, and `0(1)` Space Complexity
-fn sumZero(arr: Vec<u8>) -> Vec<u8> {
-    let left: usize = 0;
-    let right: usize = arr.len() - 1_usize;
+fn sum_zero(arr: Vec<i8>) -> Vec<i8> {
+    let mut left: usize = 0;
+    let mut right: usize = arr.len() - 1_usize;
+
+    while left < right {
+        let sum = arr[left] + arr[right];
+
+        if sum == 0 {
+            println!("Left: {left} | right: {right}");
+
+            return vec![arr[left], arr[right]];
+        } else if sum > 0 {
+            // println!("Left: {left} | right: {right}");
+            right = right - 1;
+        } else {
+            left = left + 1;
+        }
+    }
+
+    vec![]
 }
