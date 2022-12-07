ac1e4b1a08233c065c3704e1842517540bd51fbe
TheCre8tor <cre8tor.alexander@gmail.com>
2022-12-07 10:28:49 +0100

    Added the definition of space complexity

--- i1_big_o/src/space_complexity.rs
+++ i1_big_o/src/space_complexity.rs
@@ -1,4 +1,10 @@
-/// The space complexity of this function is constant space 
+/* EXPLANATION:
+ * The space Complexity of an algorithm is the total amount
+ * of memory space used by the algorithm including the space
+ * of its input values for execution.
+ */
+
+/// The space complexity of this function is constant space
 /// -> O(1) space!
 /// Because we are only assigning values to a primitive
 /// data type.
@@ -6,18 +12,18 @@ pub fn _sum(arr: Vec<u8>) -> u8 {
     let mut total = 0;
 
     for i in 0..arr.len() {
-        total += arr[i]; 
+        total += arr[i];
     }
 
     total
 }
 
-/// The space complexity of this function is constant space 
+/// The space complexity of this function is linear space
 /// -> O(n) space!
 /// Because we are assigning values to a compound data type.
 pub fn _double(arr: Vec<u8>) -> Vec<u8> {
     let mut new_arr: Vec<u8> = Vec::new();
-    
+
     for i in 0..arr.len() {
         new_arr.push(2 * arr[i]);
     }
@@ -32,19 +38,19 @@ mod test {
     #[test]
     fn calculate_sum() {
         let num: Vec<u8> = vec![1, 2, 3, 4, 5];
-    
+
         let result = _sum(num);
-    
+
         assert_eq!(result, 15);
     }
-    
+
     #[test]
     fn calculate_double() {
         let data: Vec<u8> = vec![1, 2, 3];
         let compare: Vec<u8> = vec![2, 4, 6];
-    
+
         let result = _double(data);
-    
+
         assert_eq!(result, compare);
     }
 }
